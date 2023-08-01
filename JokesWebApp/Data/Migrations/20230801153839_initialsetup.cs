4726a7b886b4390a9de8679a177b9b55d44394d9
sd_dissanayaka <95671286+SDDISSANAYAKA@users.noreply.github.com>
2023-08-01 12:24:02 -0700

    Add project files.

---
+++ JokesWebApp/Data/Migrations/20230801153839_initialsetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    JokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public String Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+        public Joke()
+        {
+                
+        }
+    }
+}
